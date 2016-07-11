0b85668c432a328d9c32e9d001d076e7806dcd01
Christian Weiss <c.weiss@c3-ls.com>
2016-07-11 16:37:48 +0200

    dependency injection config

--- src/Meceqs/MeceqsServiceCollectionExtensions.cs
+++ src/Meceqs/MeceqsServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Meceqs;
+using Meceqs.Sending;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -7,9 +8,12 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IServiceCollection AddMeceqs(this IServiceCollection services)
         {
+            // Common services
             services.TryAddSingleton<IEnvelopeFactory, DefaultEnvelopeFactory>();
-
             services.TryAddSingleton<IEnvelopeTypeLoader, DefaultEnvelopeTypeLoader>();
+
+            // Sending
+            services.TryAddSingleton<IMessageCorrelator, DefaultMessageCorrelator>();
 
             return services;
         }
