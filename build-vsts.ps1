5dfe83f8330ad30d1079a0a24b8f9a52b39c70e0
Wolfgang Kniedl <w.kniedl@c3-ls.com>
2016-09-01 15:33:15 +0200

    Build-File für VsTs erstellt

---
+++ build-vsts.ps1
@@ -0,0 +1,6 @@
+Import-Module ..\..\tools\psake\psake.psm1
+Invoke-psake .\build-definition.ps1 -properties @{
+    BuildNumber = $env:BUILD_BUILDNUMBER;
+    BuildConfiguration = $env:BuildConfiguration;
+    ArtifactsPath = $env:BUILD_ARTIFACTSTAGINGDIRECTORY 
+}
