e8dd0281b54b7382c42998697695dc28f5520da5
Christian Weiss <c.weiss@c3-ls.com>
2016-09-03 19:29:27 +0200

    rename

--- src/Meceqs/Configuration/MeceqsServiceCollectionExtensions.cs
+++ src/Meceqs/Configuration/MeceqsServiceCollectionExtensions.cs
@@ -5,6 +5,9 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class MeceqsServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds core services and returns a builder that allows to add further Meceqs-specific functionality.
+        /// </summary>
         public static IMeceqsBuilder AddMeceqs(this IServiceCollection services)
         {
             Check.NotNull(services, nameof(services));
