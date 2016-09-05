8ba9a3ad69eea976c00ac80f78be7c7ad4c8cbfd
Christian Weiss <c.weiss@c3-ls.com>
2016-09-05 21:56:02 +0200

    samples renamed

---
+++ samples/Sales.Hosts.ProcessCustomerEvents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sales.Hosts.ProcessCustomerEvents
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
