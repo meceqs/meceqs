a898539aa712010d43f828ba7b5a7a141f59e812
Christian Weiss <christian@chwe.at>
2017-08-15 11:46:32 +0200

    version bump to 0.5.0; script for updating all packages

---
+++ Update-Packages.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = "Stop"
+
+Get-ChildItem -Filter *.csproj -Recurse -Depth 10 | Foreach-Object {
+
+    $projectPath = $_.FullName
+    Write-Host "Processing $projectPath"
+
+    $projectXml = [XML](Get-Content $projectPath)
+    $projectXml.SelectNodes("/Project/ItemGroup/PackageReference") | Foreach-Object {
+
+        $packageName = $_.Include
+
+        Write-Host "- Package $packageName"
+
+        dotnet add $projectPath package $packageName
+        #if ($LASTEXITCODE -ne 0) { throw "error" }
+    }
+}
